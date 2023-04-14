f4da420f11cc570711f39a85da0af38fcbeab5c8
Robert Lynch <relynch10@gmail.com>
2023-04-14 16:11:02 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,5 @@
+const divide = (parameter1, parameter2) => parameter1 / parameter2
+
+const square = parameter1 => Math.pow(parameter1, 2)
+
+const add = (parameter1, parameter2) => parameter1 + parameter2
